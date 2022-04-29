ae8d5afa60fc4d98a28ce62fcb64bd04fd8b1051
kingqin <2464636329@qq.com>
2022-04-29 09:28:53 +0800

    '删除了文件'

--- 01 a.js
+++
@@ -1,4 +0,0 @@
-console.log('user');
-console.log('abc');
-console.log('sky');
-console.log('aaaaaaaa')
--- 03c.js
+++
@@ -1,3 +0,0 @@
-console.log('3333333333')
-console.log('4444444444');
-console.log('22222222222');
