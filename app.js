1e9a0f0f7a57ff33200accd1ae61519ef601a7a2
dudu <2464636329@qq.com>
2022-05-12 11:16:01 +0800

    layout布局已完成

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // 装包 npm install express art-template bootstrap@3.3.5 express-art-template express-session mongoose jquery@2.2.0
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+
+const app = express();
+
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.engine('.html', require('express-art-template'));
+
+
+app.get("/index",(req,res)=>{
+    res.render("index.html")
+})
+
+app.listen(3000,()=>{
+    console.log('服务器端口已启动！');
+})
---
+++ js/index.js
@@ -0,0 +1,99 @@
+$(function () {
+    var data = [
+        {
+            pcUrl: "../images/lun1.jpg",
+        },
+        {
+            pcUrl: "../images/lun2.jpg",
+        },
+        {
+            pcUrl: "../images/lun2.jpg",
+        },
+    ];
+
+    var $ol = $(".ML_banner .carousel-indicators");
+    var $inner = $(".ML_banner .carousel-inner");
+    function render() {
+        var str = "";
+        data.forEach((item, idx) => {
+            str += `<div class="item ${idx == 0 ? "active" : ""}"><a href="#" class="m_imgBox"><img src="${item.pcUrl}" alt=""></a></div>`;
+        });
+        $inner.html(str);
+
+        var str2 = "";
+        data.forEach((item, idx) => {
+            str2 += `<li data-target="#carousel-example-generic" class="${idx == 0 ? "active" : ""}" data-slide-to="${idx}"></li>`;
+        });
+        $ol.html(str2);
+
+    }
+    render();
+
+    $(document).ready(
+        function () {
+            $(".header").affix({
+                offset: {
+                    top: 100,
+                }
+            });
+            //每当加钉子执行的事件
+            $(".header").on("affixed.bs.affix", function () {
+                $("body").css("paddingTop", 180)
+            })
+            //每当取消钉子执行的事件
+            $(".header").on("affixed-top.bs.affix", function () {
+                $("body").css("paddingTop", 0)
+            })
+        }
+    );
+
+
+});
+var ul1 = document.getElementById("new_one");
+    var ul2 = document.getElementById("new_two");
+    var box = document.getElementById("mod6_main");
+    window.onload = roll(1500);
+    ul2.innerHTML = ul1.innerHTML;
+    function roll(t) {
+
+        box.scrollTop = 0;
+        var timer = setInterval(rollStart, t);
+        box.onmouseover = function () {
+            clearInterval(timer)
+        }
+        box.onmouseout = function () {
+            timer = setInterval(rollStart, t);
+        }
+    }
+
+    function rollStart() {
+        if (box.scrollTop >= ul1.scrollHeight) {
+            box.scrollTop = 0;
+        } else {
+            box.scrollTop += 10;
+            box.scrollTop += 10;
+            box.scrollTop += 15;
+        }
+    }
+    $(function () {
+        $(".navbar-toggle").click(function () {
+            $(".ML_navbar_mask ").attr("style", "display:block");
+            $(".col_right").addClass("left0");
+        });
+        $(".ML_navbar_mask").click(function () {
+            $(".ML_navbar_mask ").attr("style", "display:none");
+            $(".col_right").removeClass("left0");
+        });
+        $(".glyphicon.glyphicon-menu-down").click(function () {
+            var that = $(this);
+            if(that.hasClass("glyphicon-menu-down")){
+                that.parent().parent().next().attr("style", "display:block");
+                that.removeClass("glyphicon-menu-down");
+                that.addClass("glyphicon-menu-up");
+            }else{
+                that.removeClass("glyphicon-menu-up");
+                that.addClass("glyphicon-menu-down");
+                that.parent().parent().next().attr("style", "display:none");
+            }
+        })
+    });
